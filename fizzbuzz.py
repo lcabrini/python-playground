55eb2c9ce9f05b1fa1108ea7fa83765617bbbfba
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-30 21:23:09 +0000

    Fizzbuzz allows passing in fizz and buzz via command-line options

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,8 @@
 import sys
 
 upto = 100
+fizz = 3
+buzz = 5
 
 if len(sys.argv) > 1:
     try:
@@ -12,13 +14,30 @@ if len(sys.argv) > 1:
         sys.exit(1)
     if upto < 1:
         print(f"Not a valid value for upto: {sys.argv[1]}")
+        sys.exit(1)
+if len(sys.argv) > 2:
+    try:
+        fizz = int(sys.argv[2])
+    except:
+        print(f"Not a valid value for fizz: {sys.argv[2]}")
+        sys.exit(1)
+    if fizz < 1:
+        print(f"Not a valid value for fizz: {sys.argv[2]}")
+if len(sys.argv) > 3:
+    try:
+        buzz = int(sys.argv[3])
+    except:
+        print(f"Not a valid value for buzz: {sys.argv[3]}")
+        sys.exit(1)
+    if buzz < 1:
+        print(f"Not a valid value for buzz: {sys.argv[3]}")
 
 for i in range(1, upto+1):
-    if i % 3 == 0 and i % 5 == 0:
+    if i % fizz == 0 and i % buzz == 0:
         print("fizz buzz")
-    elif i % 3 == 0:
+    elif i % fizz == 0:
         print("fizz")
-    elif i % 5 == 0:
+    elif i % buzz == 0:
         print("buzz")
     else:
         print(i)
