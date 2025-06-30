5916876d008aaf01e5f55c6718537f028aa54efc
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-30 21:47:32 +0000

    Implement raylib basic

---
+++ raylib/basic.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import pyray as pr
+
+
+pr.set_config_flags(pr.FLAG_VSYNC_HINT)
+pr.init_window(1024, 768, "Basic")
+pr.set_target_fps(60)
+
+while not pr.window_should_close():
+    pr.begin_drawing()
+    pr.clear_background(pr.BLACK)
+    pr.end_drawing()
+
+pr.close_window()
