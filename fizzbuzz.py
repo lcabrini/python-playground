89e05c480bf07e2f3bbb2f1c4102ce3df8f54cb0
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-30 21:12:44 +0000

    Fizzbuzz takes an upto command-line option

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,19 @@
 #! /usr/bin/env python
 
-for i in range(1, 101):
+import sys
+
+upto = 100
+
+if len(sys.argv) > 1:
+    try:
+        upto = int(sys.argv[1])
+    except:
+        print(f"Not a valid value for upto: {sys.argv[1]}")
+        sys.exit(1)
+    if upto < 1:
+        print(f"Not a valid value for upto: {sys.argv[1]}")
+
+for i in range(1, upto+1):
     if i % 3 == 0 and i % 5 == 0:
         print("fizz buzz")
     elif i % 3 == 0:
