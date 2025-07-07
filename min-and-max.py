c8147595fd11b6e3fa688b2c960715bad0441a40
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-07-07 13:58:56 +0000

    Print out min before max

--- min-and-max.py
+++ min-and-max.py
@@ -7,22 +7,19 @@ def min_and_max(nums):
     max_value = nums[0]
 
     for num in nums[1:]:
-        if num < min_value:
-            min_value = num
-        if num > max_value:
-            max_value = num
-
+        if num < min_value: min_value = num
+        if num > max_value: max_value = num
     return (min_value, max_value)
 
-nums = [random.randint(1, 100) for x in range(0, 10)]
+nums = [random.randint(1, 1000) for x in range(0, 10)]
 min_value, max_value = min_and_max(nums)
 print("Using min_and_max function:")
-print(f"  max: {max_value}")
 print(f"  min: {min_value}")
+print(f"  max: {max_value}")
 
 bi_min_value = min(nums)
 bi_max_value = max(nums)
 print("Using built-ins:")
-print(f"  max: {bi_max_value}")
 print(f"  min: {bi_min_value}")
+print(f"  max: {bi_max_value}")
 
