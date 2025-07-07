f8a855adff8a0a5595c72a2d286053f1ab3e2c43
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-07-07 08:17:11 +0000

    Moved min and max into a function

--- min-and-max.py
+++ min-and-max.py
@@ -2,22 +2,34 @@
 
 import random
 
-nums = [random.randint(1, 100) for x in range(0, 100)]
-min_value = 0
-max_value = 0
-started = 0
+def min_and_max(nums):
+    min_value = 0
+    max_value = 0
+    started = 0
 
-for num in nums:
-    if not started:
-        min_value = num
-        max_value = num
-        started = True
-        continue
+    for num in nums:
+        if not started:
+            min_value = num
+            max_value = num
+            started = True
+            continue
 
-    if num < min_value:
-        min_value = num
-    if num > max_value:
-        max_value = num
+        if num < min_value:
+            min_value = num
+        if num > max_value:
+            max_value = num
 
-print(f"Max: {max_value}")
-print(f"Min: {min_value}")
+    return (min_value, max_value)
+
+nums = [random.randint(1, 100) for x in range(0, 10)]
+min_value, max_value = min_and_max(nums)
+print("Using min_and_max function:")
+print(f"  max: {max_value}")
+print(f"  min: {min_value}")
+
+bi_min_value = min(nums)
+bi_max_value = max(nums)
+print("Using built-ins:")
+print(f"  max: {bi_max_value}")
+print(f"  min: {bi_min_value}")
+
